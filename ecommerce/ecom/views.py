16d900d8601e38b251e44968698deffa45ea206a
jcfrancisco0103 <jcfrancisco@student.apc.edu.ph>
2025-03-08 15:49:55 +0800

    merging all files

--- ecommerce/ecom/views.py
+++ ecommerce/ecom/views.py
@@ -227,7 +227,7 @@ def view_feedback_view(request):
 #---------------------------------------------------------------------------------
 def search_view(request):
     # whatever user write in search box we get in query
-    query = request.GET['query']
+    query = request.GET.get('query')
     products=models.Product.objects.all().filter(name__icontains=query)
     if 'product_ids' in request.COOKIES:
         product_ids = request.COOKIES['product_ids']
@@ -238,6 +238,8 @@ def search_view(request):
 
     # word variable will be shown in html when user click on search button
     word="Searched Result :"
+
+    products = models.Product.objects.all().filter(name__icontains=query)
 
     if request.user.is_authenticated:
         return render(request,'ecom/customer_home.html',{'products':products,'word':word,'product_count_in_cart':product_count_in_cart, 'search_text': query})
