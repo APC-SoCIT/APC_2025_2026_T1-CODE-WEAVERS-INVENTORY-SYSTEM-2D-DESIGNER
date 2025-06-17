afa51b74a9e8d402eb7847565ac0b57dc2dc50f3
Jan Christopher Francisco <152581275+jcfrancisco0103@users.noreply.github.com>
2025-06-17 23:49:07 +0800

    feat: enhance edit profile page with improved styling and additional regions and cities

--- ecommerce/ecommerce/urls.py
+++ ecommerce/ecommerce/urls.py
@@ -105,5 +105,6 @@ urlpatterns = [
 ] + static(settings.MEDIA_URL, document_root=settings.STATIC_ROOT) if settings.DEBUG else []
 
 
+# Serve media files during development
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
